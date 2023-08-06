24d7cb494fa344589e0ea321e93368b6a475c8e9
Camila Banizi <camibanizi@gmail.com>
2023-08-06 17:26:59 -0300

    Método addEventListener

---
+++ eventos.js
@@ -0,0 +1,5 @@
+// Al tocar el div(solo lo verde) sale una alerta
+const div = document.querySelector("div"); // selecciona al primer div que encuentre (como solo es uno selecciona el unico que hay)
+div.addEventListener("click", function () {
+	alert("Hola! Soy el div");
+});
